de8ab7b9feee0e3bc59b66fa2be8945254079329
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-06-29 22:04:22 -0400

    started project

--- app.js
+++ app.js
@@ -2,3 +2,37 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+const books = document.getElementsByClassName("book");
+
+class book {
+    constructor (Title, Author, read) {
+        this.Title = "Title";
+        this.Author = "Author";
+        this.read = read;
+    }
+}
+
+const libraries = document.getElementsByClassName("Library");
+
+class Library {
+    consturctor (bookCount, books, markRead, addBook, ) {
+        this.bookCount = bookCount;
+        this.books = "books";
+        this.markRead = markRead;
+        this.addBook = addBook;
+    }
+}
+
+function blah (checkBox, id="readLibraryCheckbox") {
+    if (bookdId == parmId) {
+        change the book read property to true, 
+        the `checkbox` checked attribute to true,  
+        the `checked` disabled attribute to true
+    }else {
+        dont change anything
+    };
+
+}
+
+
